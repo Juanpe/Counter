204f48c54a6246a7c9017b836de03ad41ea736aa
Juan Navas <juannavas7@gmail.com>
2017-10-12 14:06:22 +0200

    feat: improved Counter syntax

--- Counter/Classes/Counter.swift
+++ Counter/Classes/Counter.swift
@@ -99,27 +99,22 @@ public class Counter{
     }
     
     fileprivate func notifyDelegateWill(oldValue: Int, newValue: Int) {
-        if let delegate = delegate {
-            milestones.forEach{ milestone in
-                guard !self.handledMilestones.contains(milestone) else { return }
-                if newValue >= milestone {
-                    delegate.counter(self, willReachValue: milestone)
-                }
-            }
+        guard let delegate = delegate else { return }
+        milestones.forEach{ milestone in
+            guard !self.handledMilestones.contains(milestone), newValue >= milestone else { return }
+            delegate.counter(self, willReachValue: milestone)
         }
     }
     
     fileprivate func notifyDelegate() {
-        if let delegate = delegate {
-            delegate.counter(self, didChangeValue: value)
-            milestones.forEach{ milestone in
-                guard !self.handledMilestones.contains(milestone) else { return }
-                if value >= milestone {
-                    delegate.counter(self, hasReachedValue: milestone)
-                    self.handledMilestones.insert(milestone)
-                }
-            }
+        guard let delegate = delegate else { return }
+        delegate.counter(self, didChangeValue: value)
+        milestones.forEach{ milestone in
+            guard !self.handledMilestones.contains(milestone), value >= milestone else { return }
+            delegate.counter(self, hasReachedValue: milestone)
+            self.handledMilestones.insert(milestone)
         }
+        
     }
 }
 
